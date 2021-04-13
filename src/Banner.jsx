a21fca10c16ec7b7453c859b388aac8240c3995d
kema-ray <rachelkemuma99@gmail.com>
2021-04-13 07:42:57 +0300

    implemented banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from "@material-ui/core";
+import Search from './Search';
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className='banner'>
+    <div className='banner_search'>
+    {showSearch && <Search />}
+         <Button onClick={() => setShowSearch(!showSearch)} className='banner_searchButton' variant='outlined'>Search Dates</Button>
+    </div>
       <div className='banner_info'>
         <h1>Get out and stretch your imagination</h1>
         <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
