bd3d2560a4ac6ebfed80ebf0aa655b8cfbc67534
kema-ray <rachelkemuma99@gmail.com>
2021-04-14 11:36:05 +0300

    implemented search component

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,4 +1,4 @@
-import React,{ usestate} from 'react';
+import React,{ useState} from 'react';
 import './Search.css'
 import "react-date-range/dist/styles.css"; //main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
